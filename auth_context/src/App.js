c5237d20cbcfb6dc79bc9e5ecbdbb1defb80fc79
harshpagar <72097667+harshpagar@users.noreply.github.com>
2022-01-21 20:27:07 +0530

    add
    add

---
+++ auth_context/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Navbar } from './components/Navbar';
+import { UserStatus } from './components/UseStatus';
+
+function App() {
+  return (
+    <div className="App">
+    <Navbar />
+    <UserStatus />
+  </div>
+  );
+}
+
+export default App;
---
+++ auth_context/src/contexts/AuthContext.jsx
@@ -0,0 +1,17 @@
+const { createContext, useState } = require("react");
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const [isAuth, setAuth] = useState(false);
+  const [token, setToken] = useState("abcde")
+
+  const toggleAuth = () => {
+    setAuth((p)=>!p);
+  };
+  return (
+    <AuthContext.Provider value={{ isAuth, toggleAuth , token, setToken}}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
