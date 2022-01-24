92311998c0d2b6b507445fd3ff43e22f9236c532
harshpagar <72097667+harshpagar@users.noreply.github.com>
2022-01-24 10:57:30 +0530

    add
    add

---
+++ private_routes/src/components/Login.jsx
@@ -0,0 +1,24 @@
+
+import { useState } from "react/cjs/react.development";
+
+export const Login = () => {
+  const [formData, setFormData] = useState({});
+
+  const handleChange = (e) => {
+    setFormData({...formData,
+   }) 
+  };
+
+  return (
+    <div>
+      <h3> Login </h3>
+      <input type="text" placeholder="Enter email" onChange={handleChange} />
+      <input
+        type="password"
+        placeholder="Enter password"
+        onChange={handleChange}
+      />
+      <button> Login </button>
+    </div>
+  );
+};
